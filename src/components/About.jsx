2580c313fafccd4d7af0b95af0518cced1f2bcbf
Akhilesh Kushwaha <akhileshkrkushwaha12.com>
2024-03-28 23:59:17 +0530

    title changed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <div
       name="About"
-      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-16"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
     >
       <div>
         <h1 className="text-3xl font-bold mb-5">About</h1>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
     <>
       <div
         name="Home"
-        className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+        className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-28"
       >
         <div className="flex flex-col md:flex-row">
           <div className="md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1">
